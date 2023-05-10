dba5b9756e878915e8e42f7e1753e12593e521d1
M.Miftakhudin <mmiftakh18@gmail.com>
2023-05-10 21:48:53 +0700

    ubah folder vendor to plugin

--- app/Cells/view_cell/panggil_js_cell.php
+++ app/Cells/view_cell/panggil_js_cell.php
@@ -1,21 +1,21 @@
 <!-- Bootstrap core JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
 <!-- Page level plugins -->
-<!-- <script src="<?= base_url('assets/'); ?>vendor/chart.js/Chart.min.js"></script> -->
-<!-- <script src="<?= base_url('assets/'); ?>vendor/chart.js/Chart.js"></script> -->
-<script src="<?= base_url('assets/'); ?>vendor/chart.js/Chart.bundle.min.js"></script>
-<script src="<?= base_url('assets/'); ?>vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="<?= base_url('assets/'); ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
-<script src="<?= base_url('assets/'); ?>vendor/sweetalert2/sweetalert2.all.min.js"></script>
+<!-- <script src="<?= base_url('assets/'); ?>plugin/chart.js/Chart.min.js"></script> -->
+<!-- <script src="<?= base_url('assets/'); ?>plugin/chart.js/Chart.js"></script> -->
+<script src="<?= base_url('assets/'); ?>plugin/chart.js/Chart.bundle.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/sweetalert2/sweetalert2.all.min.js"></script>
 
 <!-- Page level custom scripts -->
-<script src="<?= base_url('assets/'); ?>vendor/select2/select2.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugin/select2/select2.min.js"></script>
 <!-- <script src="<?= base_url('assets/'); ?>js/demo/chart-pie-demo.js"></script> -->
