fe556c99f40e6d1d7225c639c88b5f0ee8a27e2d
miftakh <mmiftakh18@gmail.com>
2023-06-05 06:10:59 +0700

    memperbaiki image yang susah tampil di dompdf

--- app/Controllers/CtrReport.php
+++ app/Controllers/CtrReport.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use Dompdf\Dompdf;
+// use Dompdf\Options;
+// use Mpdf\Mpdf;
 use App\Models\ListBarang;
 use App\Models\DListBarang;
+
+
 
 class CtrReport extends BaseController
 {
@@ -21,19 +25,51 @@ class CtrReport extends BaseController
     }
     public function barang_masuk($id = null)
     {
+        // $options = new Options();
+        // $options->set('chroot', 'realpath');
 
         $data['utama'] =  $this->ListB->find($id);
         $data['detail'] =  $this->dListB->where('mbid', $id)->findAll();
-
+        $data['gambar_apr'] = $this->imgtoBs64(ROOTPATH . '/public/assets/img/approve.svg');
         $dompdf = new Dompdf();
 
-        // $html  = view('reports/report_barang');
-        $filename = date('y-m-d-H-i-s') . '-qadr-labs-report';
+        // // $html  = view('reports/report_barang');
+        $filename = date('y-m-d-H-i-s') . '-report';
         $data['title'] = 'TANDA TERIMA BARANG';
         $html  = view('reports/report_barang', $data);
         $dompdf->loadHtml($html);
         $dompdf->setPaper('A4', 'potrait');
         $dompdf->render();
         $dompdf->stream($filename . '.pdf', ["Attachment" => false]);
+        // require_once base_url() . 'vendor/autoload.php';
+        // $mpdf = new Mpdf();
+        // // $html = view('html_to_pdf', []);
+        // $mpdf->WriteHTML($html);
+        // $this->response->setHeader('Content-Type', 'application/pdf');
+        // $mpdf->Output('arjun.pdf', 'I'); // opens in browser
+    }
+
+    //    gagal
+    private function imageToBase64($path)
+    {
+
+        $path  = $path;
+        $type  = pathinfo($path, PATHINFO_EXTENSION);
+        $data  = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64' . base64_encode($data);
+        return $base64;
+    }
+
+    private function imgtoBs64($iamge_path = false, $image_type = 'png')
+    {
+        if ($iamge_path) {
+            $img_data = fopen($iamge_path, 'rb');
+            $img_size = filesize($iamge_path);
+            $binary_image = fread($img_data, $img_size);
+            fclose($img_data);
+            $img_src = "data:image/" . $image_type . ";base64," . str_replace("\n", "", base64_encode($binary_image));
+            return $img_src;
+        }
+        return false;
     }
 }
--- app/Views/reports/report_barang.php
+++ app/Views/reports/report_barang.php
@@ -1,93 +1,130 @@
-    <?= view_cell('PanggilPluginAll::pluginCss'); ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <style>
-        * {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 14pt;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
 
-        table {
-            width: 100%;
-        }
+        <?= view_cell('PanggilPluginAll::pluginCss'); ?>
 
-        table tr td {
-            padding-left: 5px;
-            padding-right: 5px;
-        }
+        <style>
+            * {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 14pt;
+            }
 
-        table tr th {
-            padding-left: 5px;
-            padding-right: 5px;
-        }
-    </style>
-    <hr>
-    <center>
-        <h2><?= $title; ?></h2>
-    </center>
-    <table border="0">
-        <tr>
-            <td width="15%">Hari</td>
-            <td width="2%">:</td>
-            <td><?= $utama['hari']; ?></td>
-            <td width="15%">Tanggal</td>
-            <td width="2%">:</td>
-            <td><?= date('d-m-Y', strtotime($utama['tanggal'])) ?></td>
-        </tr>
-        <tr>
-            <td width="15%">Jam</td>
-            <td width="2%">:</td>
-            <td><?= $utama['jam'] . ':' . $utama['menit'] . '  WIB'; ?></td>
-            <td width="15%">No</td>
-            <td width="2%">:</td>
-            <td>-</td>
-        </tr>
-        <tr>
-            <td width="15%">Dari</td>
-            <td width="2%">:</td>
-            <td colspan="4"><?= $utama['dari']; ?></td>
-        </tr>
-        <tr>
-            <td width="15%">Untuk</td>
-            <td width="2%">:</td>
-            <td colspan="4"><?= $utama['untuk']; ?></td>
-        </tr>
-    </table>
-    <br>
+            table {
+                width: 100%;
+            }
 
-    <table border="1" cellpadding="0" cellspacing="0">
-        <tr>
-            <th width="2%">No.</th>
-            <th>Nama Barang</th>
-            <th width="2%">Jumlah</th>
-        </tr>
-        <?php foreach ($detail as $key => $row) : ?>
+            table tr td {
+                padding-left: 5px;
+                padding-right: 5px;
+            }
+
+            table tr th {
+                padding-left: 5px;
+                padding-right: 5px;
+            }
+        </style>
+    </head>
+
+    <body>
+
+
+
+
+        <hr>
+        <center>
+            <h2><?= $title; ?></h2>
+        </center>
+        <table border="0">
             <tr>
-                <td> <?= $key + 1 ?></td>
-                <td> <?= $row['nama_barang'] ?></td>
-                <td> <?= $row['jumlah'] ?></td>
+                <td width="15%">Hari</td>
+                <td width="2%">:</td>
+                <td><?= $utama['hari']; ?></td>
+                <td width="15%">Tanggal</td>
+                <td width="2%">:</td>
+                <td><?= date('d-m-Y', strtotime($utama['tanggal'])) ?></td>
             </tr>
-        <?php endforeach; ?>
-    </table>
-    <br><br><br>
-    <table>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td width="33.4%">
-                <center>(<?= $utama['pemberi']; ?>)</center>
-                <hr> No.Hp :<?= $utama['no_pemberi']; ?>
-            </td>
-            <td width="33.4%">
-                <center>(<?= $utama['penerima']; ?>)</center>
-                <hr> No.Hp :<?= $utama['no_penerima']; ?>
-            </td>
-            <td width="33.4%">
-                <center>
-                    (<?= $utama['penyedia']; ?>)</center>
-                <hr> No.Hp :<?= $utama['no_penyedia']; ?>
-            </td>
-        </tr>
-    </table>
+            <tr>
+                <td width="15%">Jam</td>
+                <td width="2%">:</td>
+                <td><?= $utama['jam'] . ':' . $utama['menit'] . '  WIB'; ?></td>
+                <td width="15%">No</td>
+                <td width="2%">:</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td width="15%">Dari</td>
+                <td width="2%">:</td>
+                <td colspan="4"><?= $utama['dari']; ?></td>
+            </tr>
+            <tr>
+                <td width="15%">Untuk</td>
+                <td width="2%">:</td>
+                <td colspan="4"><?= $utama['untuk']; ?></td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" cellpadding="0" cellspacing="0">
+            <tr>
+                <th width="2%">No.</th>
+                <th>Nama Barang</th>
+                <th width="2%">Jumlah</th>
+            </tr>
+            <?php foreach ($detail as $key => $row) : ?>
+                <tr>
+                    <td> <?= $key + 1 ?></td>
+                    <td> <?= $row['nama_barang'] ?></td>
+                    <td> <?= $row['jumlah'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <br><br><br>
+        <table>
+            <tr>
+                <td>
+                    <?php if (!empty($utama['pemberi'])) : ?>
+                        <center>
+                            <img src="<?= $gambar_apr ?>" alt="" width="200px">
+                        </center>
+                    <?php endif ?>
+                </td>
+                <td>
+                    <?php if (!empty($utama['penerima'])) : ?>
+                        <center>
+                            <img src="<?= $gambar_apr ?>" alt="" width="200px">
+                        </center>
+                    <?php endif ?>
+                </td>
+                <td>
+                    <?php if (!empty($utama['penyedia'])) : ?>
+                        <center>
+                            <img src="<?= $gambar_apr ?>" alt="" width="200px">
+                        </center>
+                    <?php endif ?>
+                </td>
+            </tr>
+            <tr>
+                <td width="33.4%">
+                    <center>(<?= $utama['pemberi']; ?>)</center>
+                    <hr> No.Hp :<?= $utama['no_pemberi']; ?>
+                </td>
+                <td width="33.4%">
+                    <center>(<?= $utama['penerima']; ?>)</center>
+                    <hr> No.Hp :<?= $utama['no_penerima']; ?>
+                </td>
+                <td width="33.4%">
+                    <center>
+                        (<?= $utama['penyedia']; ?>)</center>
+                    <hr> No.Hp :<?= $utama['no_penyedia']; ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+
+    </html>
